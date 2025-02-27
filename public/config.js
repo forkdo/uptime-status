ac09cf7df842bd25654326cfd4228fd2ee156c18
Jetsung Chan <jetsungchan@gmail.com>
2025-02-28 00:58:23 +0800

    feat: 支持 GitHub Action 发布到 CloudFlare Pages

--- public/config.js
+++ public/config.js
@@ -1,15 +1,11 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: 'iDEV 网站监测',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
   ],
 
   // 日志天数
@@ -21,16 +17,16 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: '爱开发社区',
+      url: 'https://forum.idev.top'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      url: 'https://github.com/jetsung'
     },
     {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
+      text: 'GitCode',
+      url: 'https://gitcode.com/jetsung'
     },
   ],
 };
